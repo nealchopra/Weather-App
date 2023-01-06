3e590e87117894bb82ff84e6f8bb8540645a5176
Neal Chopra <nealchopra06@gmail.com>
2023-01-05 21:46:38 -0800

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Neal Chopra on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
